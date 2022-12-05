4bd744426365c89ee8048bef98950d1142665157
Ciepol <adamulicho@gmail.com>
2022-12-05 01:36:46 +0100

    InputLetter

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
-import { useState,useEffect} from "react";
-import "./App.css";
-import WordleGrid from "./components/WordleGrid";
+import { useState, useEffect, useCallback } from 'react'
+import './App.css'
+import WordleGrid from './components/WordleGrid'
 
 export default function App() {
-  const [word, setWord] = useState("");
+  const [word, setWord] = useState('')
+  const maxNumbersOfLetters = 10
 
-  useEffect(()=>{
-    document.addEventListener("keydown", detectKeyDown,true)},[])
-
-  const detectKeyDown = (event) =>{
-      if(event.repeat) return
-    console.log(event)
-    setWord((prevWord) => prevWord + event.key)
+  const enterIsClicked = () => {
+    console.log('Enter clicked')
   }
+
+  const backspaceIsClicked = () => {
+    setWord((prevWord) => prevWord.slice(0, -1))
+    console.log('Enter clicked')
+  
+  }
+
+  const detectKeyDown = useCallback(
+    (event) => {
+      console.log(event)
+      if (event.key === 'Enter') enterIsClicked()
+      if (event.key === 'Backspace'&&word) backspaceIsClicked() 
+      if (event.key === 'Delete') backspaceIsClicked()
+      if (event.keyCode < 65 || event.keyCode > 90) return
+      if (word.length > maxNumbersOfLetters - 1) return
+      setWord((prevWord) => prevWord + event.key.toLowerCase())
+    },
+    [word]
+  )
+
+  useEffect(() => {
+    document.addEventListener('keydown', detectKeyDown)
+    return () => document.removeEventListener('keydown', detectKeyDown)
+  }, [word.length, detectKeyDown])
+
   return (
     <div className="App">
-      <WordleGrid letters={word}/>
+      <WordleGrid letters={word} numbersOfLetters={maxNumbersOfLetters} />
     </div>
-  );
+  )
 }
